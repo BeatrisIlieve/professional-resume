518afa4c0a10cbb0f457af0350fb3d52018e93b6
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-26 18:37:17 +0300

    Updated resume

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,14 +1,16 @@
+import styles from "./Main.module.css";
+
 export const Main = () => {
   return (
     <ul className={styles["list"]} role="list">
-      <li className={styles["list-item-more-pad"]}>
+      <li className={styles["list-item"]}>
         Built with <b>MongoDB</b>, <b>Express</b>.js, <b>React</b> and{" "}
         <b>Node</b>.js
       </li>
-      <li className={styles["list-item-more-pad"]}>
+      <li className={styles["list-item"]}>
         Hosted on <b>Amazon Web Services</b> EC2 Instance
       </li>
-      <li className={`${styles["list-item-more-pad"]}`}>
+      <li className={`${styles["list-item"]}`}>
         <div className={`${styles["testing-wrapper"]}`}>
           <span>Testing - Jest</span>
           <span>
@@ -23,120 +25,27 @@ export const Main = () => {
               alt="Coverage Status"
             />
           </span>
-          <span>
-            (<b>AI</b> has been a great help)
-          </span>
         </div>
       </li>
-      <li className={styles["list-item-more-pad"]}>
+      <li className={styles["list-item"]}>
         Optimized performance with React's <b>useMemo</b>, <b>useCallback</b>,
         and <b>memo</b>
       </li>
-      <li className={styles["list-item-more-pad"]}>
+      <li className={styles["list-item"]}>
         Custom <b>Forms</b>
       </li>
-      <li className={styles["list-item-more-pad"]}>
+      <li className={styles["list-item"]}>
         Input Validation providing <b>immediate feedback before submission</b>
       </li>
-      <li className={styles["list-item-more-pad"]}>
+      <li className={styles["list-item"]}>
         Error Handling delivering notifications for <b>backend</b> issues
       </li>
-      <li className={styles["list-item-more-pad"]}>
+      <li className={styles["list-item"]}>
         <b>Database aggregations</b> for Product Filtering and Real-Time
         Availability Tracking
       </li>
-      <li className={styles["list-item-more-pad"]}>
+      <li className={styles["list-item"]}>
         <b>Responsive Design</b> - fully optimized for all device sizes
-        <div className={styles["grid-container"]}>
-          <a
-            href="https://res.cloudinary.com/deztgvefu/image/upload/v1726649931/mobile/login_cpsdjd.png"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="https://res.cloudinary.com/deztgvefu/image/upload/v1726649931/mobile/login_cpsdjd.png"
-              alt=""
-              className={styles["mobile-images-style"]}
-            />
-          </a>
-          <a
-            href="https://res.cloudinary.com/deztgvefu/image/upload/v1726649932/mobile/home_airjiz.png"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="https://res.cloudinary.com/deztgvefu/image/upload/v1726649932/mobile/home_airjiz.png"
-              alt=""
-              className={styles["mobile-images-style"]}
-            />
-          </a>
-          <a
-            href="https://res.cloudinary.com/deztgvefu/image/upload/v1726649931/mobile/collection_phoiru.png"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="https://res.cloudinary.com/deztgvefu/image/upload/v1726649931/mobile/collection_phoiru.png"
-              alt=""
-              className={styles["mobile-images-style"]}
-            />
-          </a>
-          <a
-            href="https://res.cloudinary.com/deztgvefu/image/upload/v1726649930/mobile/item_c6b12l.png"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="https://res.cloudinary.com/deztgvefu/image/upload/v1726649930/mobile/item_c6b12l.png"
-              alt=""
-              className={styles["mobile-images-style"]}
-            />
-          </a>
-          <a
-            href="https://res.cloudinary.com/deztgvefu/image/upload/v1726649930/mobile/bag_rc7nkx.png"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="https://res.cloudinary.com/deztgvefu/image/upload/v1726649930/mobile/bag_rc7nkx.png"
-              alt=""
-              className={styles["mobile-images-style"]}
-            />
-          </a>
-          <a
-            href="https://res.cloudinary.com/deztgvefu/image/upload/v1726649930/mobile/order-confirmation_umr84p.png"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="https://res.cloudinary.com/deztgvefu/image/upload/v1726649930/mobile/order-confirmation_umr84p.png"
-              alt=""
-              className={styles["mobile-images-style"]}
-            />
-          </a>
-          <a
-            href="https://res.cloudinary.com/deztgvefu/image/upload/v1726649929/mobile/account_qdrxfh.png"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="https://res.cloudinary.com/deztgvefu/image/upload/v1726649929/mobile/account_qdrxfh.png"
-              alt=""
-              className={styles["mobile-images-style"]}
-            />
-          </a>
-          <a
-            href="https://res.cloudinary.com/deztgvefu/image/upload/v1726649930/mobile/wishlist_fwclxh.png"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="https://res.cloudinary.com/deztgvefu/image/upload/v1726649930/mobile/wishlist_fwclxh.png"
-              alt=""
-              className={styles["mobile-images-style"]}
-            />
-          </a>
-        </div>
       </li>
     </ul>
   );

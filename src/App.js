9cc29738b17f58a6c6d6254f86651183d0fb946e
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-26 16:25:42 +0300

    Added layout

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import "normalize.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <section className={styles["resume"]}>
+      <div className={styles["content"]}>
+        <section className={styles["about"]}>About</section>
+        <section className={styles["project"]}>Project</section>
+        <section className={styles["education"]}>education</section>
+        <section section className={styles["footer"]}>
+          <img
+            alt="React"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+          />
+          <img
+            alt="Jest"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jest/jest-plain.svg"
+          />
+          <img
+            alt="GithubActions"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/githubactions/githubactions-original.svg"
+          />
+          <img
+            alt="AWS"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg"
+          />
+          <img
+            alt="Docker"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"
+          />
+          <img
+            alt="JavaScript"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+          />
+          <img
+            alt="Express"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg"
+          />
+          <img
+            alt="NodeJS"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg"
+          />
+          <img
+            alt="MongoDB"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg"
+          />
+          <img
+            alt="Mongoose"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongoose/mongoose-original.svg"
+          />
+          <img
+            alt="Handlebars"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/handlebars/handlebars-line.svg"
+          />
+          <img
+            alt="Python"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"
+          />
+          <img
+            alt="Django"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/django/django-plain-wordmark.svg"
+          />
+          <img
+            alt="PostgreSQL"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg"
+          />
+          <img
+            alt="HTML"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+          />
+          <img
+            alt="CSS"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+          />
+          <img
+            alt="Pycharm"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pycharm/pycharm-original.svg"
+          />
+          <img
+            alt="Visual Studio Code"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"
+          />
+        </section>
+      </div>
+    </section>
+  );
+}
+
+export default App;
